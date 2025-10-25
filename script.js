98fdfd63de364fc4dbcd6588ecf825f7b57ee93c
Somesh Farkya <someshfarkya91@gmail.com>
2025-10-25 14:41:34 +0530

    Add initial website files for language learning project

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.querySelectorAll('.start-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Starting learning for the selected language!');
+    });
+});
